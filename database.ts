430dd39fb742ae52275d07496513ae133d9a83fa
David Ibokette <dibokette@itscovered.com>
2022-11-04 13:56:18 -0400

    code cleanup;

--- database.ts
+++ database.ts
@@ -21,15 +21,7 @@ const pool = createPool({
 
 export function initialize() {
   return new Promise(async resolve => {
-    const x = {
-      host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_PORT),
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-    }
-    console.log(`2 pool is ${JSON.stringify(x)}`)
-    console.log("1 Creating table")
+    console.log("Creating table")
     await pool.query(`
       CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (
         ${TableColumn.Id} INT NOT NULL UNIQUE AUTO_INCREMENT,
@@ -43,26 +35,6 @@ export function initialize() {
     resolve(null)
   })
 }
-
-// export async function initialize() {
-//     const x = {
-//       host: process.env.MYSQL_HOST,
-//       port: Number(process.env.MYSQL_PORT),
-//       user: process.env.MYSQL_USER,
-//       password: process.env.MYSQL_PASSWORD,
-//       database: process.env.MYSQL_DATABASE,
-//     }
-//     console.log(`2 pool is ${JSON.stringify(x)}`)
-//     console.log("1 Creating table")
-//     await pool.query(`
-//       CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (
-//                                                  ${TableColumn.Id} INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                                                  ${TableColumn.Text} VARCHAR(255) NOT NULL,
-//                                                  ${TableColumn.Status} INT NOT NULL,
-//                                                  PRIMARY KEY (${TableColumn.Id})
-//       ) ENGINE=InnoDB;
-//     `)
-// }
 
 export async function insertTodo(text: string): Promise<Todo> {
   const [row] = await pool.query(
